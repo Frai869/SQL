c9a6e95512c97e64aa0aaff506d942ee01c9184a
Alexander <150174836+Frai869@users.noreply.github.com>
2024-07-19 10:55:09 +0500

    Update Hw-select.sql

--- Hw-select.sql
+++ Hw-select.sql
@@ -1,47 +1,47 @@
--- Название и продолжительность самого длительного трека(+):
+-- Название и продолжительность самого длительного трека:
 select name, length FROM track
 where length = (select max(length) from track);
 
---Название треков, продолжительность которых не менее 3,5 минут(+):
+--Название треков, продолжительность которых не менее 3,5 минут:
 select name, length FROM track
 where length >= '00:03:30';
 
---Названия сборников, вышедших в период с 2018 по 2020 год включительно(+):
+--Названия сборников, вышедших в период с 2018 по 2020 год включительно:
 select name, year from collection
 where year between '2018-01-01' and '2020-12-31';
 
---Исполнители, чьё имя состоит из одного слова(+):
+--Исполнители, чьё имя состоит из одного слова:
 select name from artist
 where name not like '% %';
 
---Название треков, которые содержат слово «мой» или «my»(Исправлено+):
+--Название треков, которые содержат слово «мой» или «my»:
 select name from track
 where name ilike 'my %' or name ilike '% my' or name ilike '% my %' or name ilike 'my';
 
---Другой способ выборки "my"(Добавлено+):
+--Другой способ выборки "my":
 select name from track
 where string_to_array(name, ' ') && array['My', 'my'];
 
---Другой способ выборки "my"(Добавлено+):
+--Другой способ выборки "my":
 select name from track
 where name ~* '\mmy' and name ~* 'my\M'
 
---Количество исполнителей в каждом жанре(+):
+--Количество исполнителей в каждом жанре:
 SELECT name, COUNT(*) FROM genre
 join artist_genre on genre.id = artist_genre.genre_id
 group by genre.id
 
---Количество треков, вошедших в альбомы 2019–2020 годов(Исправлено+):
+--Количество треков, вошедших в альбомы 2019–2020 годов:
 SELECT COUNT(*) from album
 join track on album.id = track.album_id 
 where album.year between '2019-01-01' and '2020-12-31';
 
---Средняя продолжительность треков по каждому альбому(+):
+--Средняя продолжительность треков по каждому альбому:
 select album.name, avg(track.length) from album
 left join track on album.id = track.album_id
 group by album.name;
 
---Все исполнители, которые не выпустили альбомы в 2020 году(Исправлено+):
+--Все исполнители, которые не выпустили альбомы в 2020 году:
 select artist.name from artist
 where artist.name not in (
 	select artist.name from artist
@@ -50,7 +50,7 @@ where artist.name not in (
 	where album.year between '2020-01-01' and '2020-12-31'
 );
 
---Названия сборников, в которых присутствует конкретный исполнитель AC/BC(+):
+--Названия сборников, в которых присутствует конкретный исполнитель AC/BC:
 select distinct collection.name, artist.name from collection
 join track_collection on collection.id = track_collection.collection_id
 join track on track.id = track_collection.track_id
